f67eaca7cf96783e12772d443188adfffc8035c9
Mikolaj Knap <mikozxc@gmail.com>
2024-10-16 09:43:56 +0200

    Poprawa przesylanego jsona paginacji

--- models/bookModel.js
+++ models/bookModel.js
@@ -4,20 +4,30 @@ exports.getBooksPaginated = async (filter, filterBy, page = 0, size = 8) => {
     const connection = await getConnection();
 
     let offset = page * size;
-    let query = `SELECT * FROM Book WHERE 1=1`;
+    let query = `SELECT BookId, Title, BookAuthor, BookCategory, ImageUrl, Description FROM Book WHERE 1=1`;
 
     if (filter && filterBy) {
         query += ` AND ${filterBy} LIKE '%${filter}%'`;
     }
 
     query += ` OFFSET ${offset} ROWS FETCH NEXT ${size} ROWS ONLY`;
+
     const result = await connection.execute(query);
+
+    const books = result.rows.map((row) => ({
+        bookId: row[0],
+        title: row[1],
+        bookAuthor: row[2],
+        bookCategory: row[3],
+        imageUrl: row[4],
+        description: row[5]
+    }));
 
     const totalQuery = `SELECT COUNT(*) FROM Book WHERE 1=1`;
     const totalResult = await connection.execute(totalQuery);
 
     return {
-        books: result.rows,
+        books,
         totalPages: Math.ceil(totalResult.rows[0] / size)
     };
 };
